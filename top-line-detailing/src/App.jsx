b6a79013f5b1dee4683ba48413569b798885f637
Patrick Riley <righthandriley32@gmail.com>
2025-07-09 18:11:56 -0400

    update

--- top-line-detailing/src/App.jsx
+++ top-line-detailing/src/App.jsx
@@ -5,10 +5,35 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="app">
-      <h1>Top Line Detailing</h1>
-      <p>Your one-stop solution for all your car detailing needs.</p>
-    </div>
+    <main>
+      <div className="app">
+        <h1 style={{
+          color: 'white',
+          fontSize: '72px',
+        }}>
+          <span>Top Line</span>
+          <span style={{ color: '#FFD700' }}> Detailing</span>
+        </h1>
+        <p style={{ color: 'white' }}>Indulge in the ultimate automotive treatment with our elite detailing services. We combine precision, passion, and premium products to transform every inch of your vehicle, delivering a showroom shine every time.
+
+</p>
+        <div className="CTA">
+          <button className="cta-button" style={{
+            padding: '15px 20px',
+            fontSize: '16px',
+            borderRadius: '4px',
+            border: 'none',
+            backgroundColor: '#FFD700',
+            color: 'black',
+            cursor: 'pointer',
+            alignSelf: 'center',
+          }}>
+            Schedule a Service Today!
+          </button>
+        </div>
+      </div>
+
+    </main>
   )
 }
 
