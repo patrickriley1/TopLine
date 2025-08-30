8de3bc05168156b5418ee570523ae3fd0af9967d
Patrick Riley <righthandriley32@gmail.com>
2025-08-30 12:30:51 -0400

    restart

---
+++ topline/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <main>
+      <div className="Header">
+        <h1>Top Line Detailing</h1>
+      </div>
+      <div className="Content">
+        <p>Welcome to Top Line Detailing! We look forward to giving you a premium detailing experience.</p>
+      </div>
+    </main>
+  )
+}
+
+export default App;
