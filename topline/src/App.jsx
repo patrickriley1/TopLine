95ad2f8c44258a39b8eb631315dd62957ae75e76
Patrick Riley <righthandriley32@gmail.com>
2025-08-31 14:52:33 -0400

    added CTA button

--- topline/src/App.jsx
+++ topline/src/App.jsx
@@ -9,7 +9,20 @@ function App() {
         <h1>Top Line Detailing</h1>
       </div>
       <div className="Content">
-        <p>Welcome to Top Line Detailing! We look forward to giving you a premium detailing experience.</p>
+        <h3>Welcome to Top Line Detailing! We look forward to giving you a premium detailing experience.</h3>
+        <button >
+          <p>Book a Detail Now!</p>
+        </button>
+      </div>
+      <div className="Testimonials">
+        <h2>See What Our Customers Are Saying!</h2>
+
+
+
+        
+      </div>
+      <div className="Footer">
+        <p>Questions? Contact us at <a href="mailto:toplinedetailing@gmail.com">toplinedetailing@gmail.com</a> or <a href="tel:+1234567890">+1 (234) 567-890</a></p>
       </div>
     </main>
   )
