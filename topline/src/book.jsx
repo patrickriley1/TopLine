bef0de4989b701f175f369f9aded36bfe8b4c290
Patrick Riley <righthandriley32@gmail.com>
2025-10-13 15:26:06 -0400

    button fix

--- topline/src/book.jsx
+++ topline/src/book.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 function Book() {
     return (
-
         <main>
             <header className="Title">
                 <div className="spacer">
@@ -11,35 +10,35 @@ function Book() {
                 </div>
                 <hr />
             </header>
-            <div className="BookTitle">
-                <Link to="/" className="backButton"><p>Back</p></Link>
-                <h2>Choose from one of our Services!</h2>
+            <div className="BookSection">
+                <div className="BookTitle">
+                    <Link to="/" className="backButton"><p>Back</p></Link>
+                    <h2>Choose from one of our Services!</h2>
+                </div>
+                <div className="flexrow">
+                    <div className="PackageCard">
+                        <h2>Basic Package</h2>
+                        <ul>
+                            <li>Exterior wash and wax</li>
+                        </ul>
+                    </div>
+                    <div className="PackageCard">
+                        <h2>Premium Package</h2>
+                        <ul>
+                            <li>Exterior wash and wax</li>
+                            <li>Interior vacuum and wipe down</li>
+                        </ul>
+                    </div>
+                    <div className="PackageCard">
+                        <h2>Ceramic Coating</h2>
+                        <ul>
+                            <li>High-quality ceramic coating application</li>
+                            <li>Long-lasting protection and shine</li>
+                        </ul>
+
+                    </div>
+                </div>
             </div>
-            <div className="flexrow">
-                <div className="PackageCard">
-                    <h2>Basic Package</h2>
-                    <ul>
-                        <li>Exterior wash and wax</li>
-                    </ul>
-                </div>
-                <div className="PackageCard">
-                    <h2>Premium Package</h2>
-                    <ul>
-                        <li>Exterior wash and wax</li>
-                        <li>Interior vacuum and wipe down</li>
-                    </ul>
-                </div>
-                <div className="PackageCard">
-                    <h2>Ceramic Coating</h2>
-                    <ul>
-                        <li>High-quality ceramic coating application</li>
-                        <li>Long-lasting protection and shine</li>
-                    </ul>
-
-                </div>
-            </div>
-
-
         </main>
     )
 }
